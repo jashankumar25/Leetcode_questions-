925c2470b12b6669d20da57b8e43d3035890d9b1
jashan kumar <jashanbansal2004@gmail.com>
2026-08-15 15:15:59 +0530

    Time: 0 ms (100%), Space: 12.1 MB (13.05%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -12,3 +12,40 @@ public:
         return str[0].substr(0, i);
     }
 };
+/*class Solution {
+public:
+    string longestCommonPrefix(vector<string>& s) {
+        string ans = "";
+        sort(s.begin(), s.end());
+
+        int i = 0;
+
+        while (i < s[0].length() && s[0][i] == s[s.size()-1][i]) {
+            ans += s[0][i];
+            i++;
+        }
+
+        return ans;
+    }
+};
+
+-----------------------------------------------
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& s) {
+        string ans = "";
+
+        for (int i = 0; i < s[0].length(); i++) {
+            for (int j = 0; j < s.size()-1; j++) {
+
+                if (s[j][i] != s[j+1][i])
+                    return ans;
+            }
+
+            ans += s[0][i];
+        }
+
+        return ans;
+    }
+};
+*/
