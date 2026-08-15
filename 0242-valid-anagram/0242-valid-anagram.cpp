b0192de592e7d938866b47705446fe9951b74dab
jashan kumar <jashanbansal2004@gmail.com>
2026-08-15 15:51:38 +0530

    Time: 0 ms (100%), Space: 9.5 MB (92.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+        return false;
+
+        int freq[26]={0};
+        for(auto ch:s){freq[ch-'a']++;}
+        for(auto ch:t){freq[ch-'a']--;}
+
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]!=0)
+            return false;
+        }
+        return true;
+    }
+};
+/* bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
 
         return s==t;
-    }
-};
+    }*/
